cd9beff764f7e05bd8a1cb7d97e82b27d56e2cd0
rednik96 <nikidev96@yahoo.de>
2017-03-08 17:08:09 +0100

    Login

---
+++ src/main/java/de/eventuell/views/LoginView.java
@@ -0,0 +1,49 @@
+package de.eventuell.views;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import de.eventuell.exceptions.LoginFailedException;
+import de.eventuell.services.UserServiceMock;
+import de.eventuell.services.Interfaces.IUserService;
+
+@ManagedBean
+@RequestScoped
+public class LoginView {
+	
+	private IUserService userService;
+	private String eMail;
+	private String password;
+	
+	public LoginView () {
+		userService = new UserServiceMock();
+	}
+
+	public String login() {
+		// login über den UserService
+		try {
+			userService.login(this.eMail, this.password);
+		} catch (LoginFailedException e) {
+			System.out.println(" --> Login Failed");
+			return "login.jsf";
+		}
+		// wenn erfolgreich user durch das Userobjekt ersetzen
+		// beim scheitern die E-Mail im anonymen user setzen damit die im Login angezeigt werden kann
+		System.out.println("Login");
+		return "index.html";
+	}
+	
+	// Getter und Setter
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
