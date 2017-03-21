8f31e91351fde1f03153a053ab7be2cf00d6e353
rednik96 <nikidev96@yahoo.de>
2017-03-21 14:50:04 +0100

    changed login to use the converter

--- src/main/java/de/eventuell/views/LoginView.java
+++ src/main/java/de/eventuell/views/LoginView.java
@@ -20,7 +20,7 @@ public class LoginView {
 	
 	@Inject
 	private IUserService userService;
-	private String eMail;
+	private User user;
 	private String password;
 	
 	public LoginView () {
@@ -28,30 +28,29 @@ public class LoginView {
 	}
 
 	public String login() {
-		User user;
-		try {
-			user = userService.login(this.eMail, this.password);
-		} catch (LoginFailedException e) {
+		System.out.println(this.user);
+		if (user != null && user.evaluateCredentials(this.password)) {
+			userSession.setUser(user);
+			return "index.jsf?faces-redirect=true";
+		} else {
 			System.out.println("----> Login Failed, but where is the message?");
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "E-mail und Paswort sind nicht gültig!", null);
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 			return "login.jsf";
 		}
-		userSession.setUser(user);
-		return "index.jsf?faces-redirect=true";
 	}
 	
 	// Getter und Setter
-	public String geteMail() {
-		return eMail;
-	}
-
-	public void seteMail(String eMail) {
-		this.eMail = eMail;
-	}
-
 	public String getPassword() {
 		return password;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	public void setPassword(String password) {
